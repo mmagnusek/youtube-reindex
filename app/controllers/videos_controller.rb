feb277c7891038a84014222f9f2160506b846b70
Ján Ondrušek <ondrusek.jan@gmail.com>
2010-12-22 13:47:29 +0100

    Default detect geolocation by client IP

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,16 +6,18 @@ class VideosController < ApplicationController
   def index
     keyword = params[:q]
     location = params[:location]
-    
-    params = {:q => keyword }
-    unless location.blank?
-      geo_location = Geokit::Geocoders::GoogleGeocoder.geocode location.removeaccents
-      params.merge!({:location => geo_location.ll})
-      p geo_location
-      @videos = VideoSearch.new(params).results_by_location(geo_location.ll)
-    else
-      @videos = VideoSearch.new(params).results
+
+    if location.blank?
+      location = Geokit::Geocoders::IpGeocoder.geocode(request.remote_ip).city
     end
+
+    logger.info "Location Keyword: " + location + " IP: " + request.remote_ip
+
+    geo_location = Geokit::Geocoders::GoogleGeocoder.geocode location.removeaccents
+    params = {:q => keyword, :location => geo_location.ll}
+    logger.info geo_location
+    p geo_location
+    @videos = VideoSearch.new(params).results_by_location(geo_location.ll)
   end
   
   def show
